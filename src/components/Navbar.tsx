e5bac371fa1c36614d78c247e1cfbb6cb14989c6
BodartA <a.bodart23@gmail.com>
2024-06-01 21:13:30 +0200

    last commit before changing the nav structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='fixed top-0 left-0 w-full flex h-[110px] pl-4 pr-4 sm:pl-10 sm:pr-10 lg:pl-16 lg:pr-16 justify-between font-dm-sans items-center shadow-sm bg-white z-50'>
+    <nav className='fixed top-0 left-0 w-full flex h-[110px] pl-4 pr-4 sm:pl-10 sm:pr-10 lg:pl-16 lg:pr-16 justify-between font-dm-sans items-center shadow-sm bg-white'>
 
       <div className='flex gap-[14px]'>
         <Ham color='#8D8BA7' className=' w-9 h-9 my-auto'></Ham>
@@ -27,7 +27,7 @@ const Navbar = () => {
         {isOpen ? <X className=' stroke-back-purple h-8 w-8' /> : <AlignRight className=' stroke-back-purple h-7 w-7' />}
       </button>
 
-      <div className={`${isOpen ? ' transition-transform -translate-x-full' : ' transition-transform translate-x-5'} lg:transition-none lg:transform-none lg:items-center lg:flex gap-6 lg:static absolute top-full left-full leading-8 h-screen lg:h-auto bg-white z-40`}>
+      <div className={`${isOpen ? ' transition-transform -translate-x-full' : ' transition-transform translate-x-5'} lg:transition-none lg:transform-none lg:items-center lg:flex gap-6 lg:static absolute top-full left-full leading-8 h-screen lg:h-auto mt-[2px] bg-white z-40`}>
         <ul className='lg:flex gap-[33px] text-back-purple'>
           <li>Home</li>
           <li>About</li>
