7eb4044aa3c749070167231e31f4bb2f510038fc
BodartA <a.bodart23@gmail.com>
2024-06-01 17:36:01 +0200

    Good responsive Navbar, the end is near.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,8 +13,9 @@ const Navbar = () => {
   }
 
   return (
-    <div className='fixed top-0 left-0 w-full flex h-[110px] pl-4 pr-4 sm:pl-10 sm:pr-10 lg:pl-16 lg:pr-16 justify-between font-dm-sans'>
-      <div className='flex my-auto gap-[14px]'>
+    <nav className='fixed top-0 left-0 w-full flex h-[110px] pl-4 pr-4 sm:pl-10 sm:pr-10 lg:pl-16 lg:pr-16 justify-between font-dm-sans items-center shadow-sm bg-white z-50'>
+
+      <div className='flex gap-[14px]'>
         <Ham color='#8D8BA7' className=' w-9 h-9 my-auto'></Ham>
         <div className='flex gap-[6px]'>
           <h1 className=' font-lilita font-bold text-4xl text-back-purple'>Food</h1>
@@ -22,17 +23,21 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className={`${isOpen? '': 'hidden'} my-auto lg:flex gap-6 absolute top-full lg:static`}>
-        <ul className='my-auto lg:flex gap-[33px] text-back-purple'>
+      <button onClick={handleClick} className={`lg:hidden items-center`}>
+        {isOpen ? <X className=' stroke-back-purple h-8 w-8' /> : <AlignRight className=' stroke-back-purple h-7 w-7' />}
+      </button>
+
+      <div className={`${isOpen ? ' transition-transform -translate-x-full' : ' transition-transform translate-x-5'} lg:transition-none lg:transform-none lg:items-center lg:flex gap-6 lg:static absolute top-full left-full leading-8 h-screen lg:h-auto bg-white z-40`}>
+        <ul className='lg:flex gap-[33px] text-back-purple'>
           <li>Home</li>
           <li>About</li>
           <li>Resources</li>
           <li>Contact</li>
         </ul>
-          <SmallPrimaryButton name={'Get Started'}></SmallPrimaryButton>
+        <SmallPrimaryButton name={'Get Started'}></SmallPrimaryButton>
       </div>
-      <button onClick={ handleClick } className={`lg:hidden my-auto`}>{isOpen? <X className=' stroke-back-purple h-8 w-8'/>:<AlignRight className=' stroke-back-purple h-7 w-7'/>}</button>
-    </div>
+
+    </nav>
   );
 };
 
--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LandingPage = () => {
   return (
-    <div>
-      
+    <div className='z-10 flex min-h-screen h-screen bg-white justify-center items-center'>
+      <div className=''>Hello</div>
     </div>
   );
 };
